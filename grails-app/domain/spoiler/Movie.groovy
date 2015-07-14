c2aa8ef6b80cef2336982e30b89b32cacefb56c6
Jonas Lins <jal3@cin.ufpe.br>
2015-07-14 00:47:36 -0300

    Movie model added

---
+++ grails-app/domain/spoiler/Movie.groovy
@@ -0,0 +1,23 @@
+package spoiler
+
+import ontology.Core
+
+class Movie {
+
+    String name
+    Date releaseDate
+    Double grade
+    String synopsis
+
+    static hasMany = [cast: Actor, directors: Director, genres: Genre]
+
+    static belongsTo = [Director, Genre]
+
+    static constraints = {
+    }
+
+    def afterInsert() {
+        Core core = Core.getInstance();
+        core.insertClassInstance("Series", id+"")
+    }
+}
